1220aaf22e2ddbd5864034e9344c52da59a0e946
codeSystemWeb <codesystemweb@gmail.com>
2026-03-20 10:11:52 -0300

    inicio site

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
+function toggleMenu() {
+  document.getElementById("mobileMenu").classList.toggle("active");
+}
 
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
+/* ANIMAÇÃO SCROLL */
+const elements = document.querySelectorAll('.fade');
+
+function showOnScroll() {
+  const trigger = window.innerHeight * 0.85;
+
+  elements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < trigger) {
+      el.classList.add('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', showOnScroll);
+
+/* CAROUSEL AUTO */
+const carousel = document.getElementById("carousel");
+
+let scrollAmount = 0;
+
+setInterval(() => {
+  scrollAmount += 310;
+
+  if (scrollAmount >= carousel.scrollWidth) {
+    scrollAmount = 0;
+  }
+
+  carousel.scrollTo({
+    left: scrollAmount,
+    behavior: "smooth"
+  });
+}, 3000);
